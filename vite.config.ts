ae280e68d7f99fbcc6b49972e0cc3937d279ff66
HARSH RAJ <84755430+harshraj0235@users.noreply.github.com>
2025-06-01 14:04:35 +0530

    Fix Googlebot Blocked by robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     minify: 'terser',
     cssMinify: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
     rollupOptions: {
       output: {
         manualChunks: {
