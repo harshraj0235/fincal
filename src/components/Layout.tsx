4c633dc8050a76d6134a99c36c6e30959fb5281e
HARSH RAJ <84755430+harshraj0235@users.noreply.github.com>
2025-06-15 21:09:15 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,65 +1,35 @@
-import React, { Suspense, lazy } from 'react';
-
+import React, { Suspense } from 'react';
 import { Header } from './Header';
-
 import { Footer } from './Footer';
-
 import { Sidebar } from './Sidebar';
-
 import { useLocation } from 'react-router-dom';
 
-
-
 interface LayoutProps {
-
-  children: React.ReactNode;
-
+  children: React.ReactNode;
 }
 
-
-
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+  const isMoneyPage = location.pathname.startsWith('/money/');
 
-  const location = useLocation();
-
-  const isHomePage = location.pathname === '/';
-
-  
-
-  return (
-
-    <div className="min-h-screen flex flex-col">
-
-      <Header />
-
-      <main className="flex-grow flex">
-
-        {!isHomePage && (
-
-          <div className="hidden md:block w-64 flex-shrink-0">
-
-            <Sidebar />
-
-          </div>
-
-        )}
-
-        <div className={`flex-grow ${isHomePage ? '' : 'p-6'}`}>
-
-          <Suspense fallback={<div className="flex justify-center items-center h-64">Loading...</div>}>
-
-            {children}
-
-          </Suspense>
-
-        </div>
-
-      </main>
-
-      <Footer />
-
-    </div>
-
-  );
-
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex">
+        {/* Show Sidebar on all pages except home and /money/* routes */}
+        {!isHomePage && !isMoneyPage && (
+          <div className="hidden md:block w-64 flex-shrink-0">
+            <Sidebar />
+          </div>
+        )}
+        <div className={`flex-grow ${isHomePage || isMoneyPage ? '' : 'p-6'}`}>
+          <Suspense fallback={<div className="flex justify-center items-center h-64">Loading...</div>}>
+            {children}
+          </Suspense>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
