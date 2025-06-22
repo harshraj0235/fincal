ea549550cf21caa727ca7717bd6d142e323b05cc
HARSH RAJ <84755430+harshraj0235@users.noreply.github.com>
2025-06-22 14:56:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,73 @@
-// src/App.tsx
-import React from "react";
-import { Layout } from "./components/Layout";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { ScrollToTop } from "./components/ScrollToTop";
-import { calculatorCategories } from "./data/calculatorData";
-import { CalculatorPage } from "./pages/CalculatorPage";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import { WriteBlog } from "./pages/WriteBlog";
-import { PrivacyPolicy } from "./pages/PrivacyPolicy";
-import { TermsAndConditions } from "./pages/TermsAndConditions";
-import { AboutUs } from "./pages/AboutUs";
-import { ContactUs } from "./pages/ContactUs";
-import { Sitemap } from "./pages/Sitemap";
-import { SitemapXml } from "./pages/SitemapXml";
-import { BankingKnowledge } from "./pages/BankingKnowledge";
-import { CategoryPage } from "./pages/CategoryPage";
-import { CreditCardFinder } from "./calculators/CreditCardFinder";
-import ExcelTool from "./pages/ExcelTool";
-import ExcelToolPost from "./pages/ExcelToolPost";
-import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
 
-const queryClient = new QueryClient();
+import { Layout } from './components/Layout';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { ScrollToTop } from './components/ScrollToTop';
+import { calculatorCategories } from './data/calculatorData';
+import { CalculatorPage } from './pages/CalculatorPage';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import { WriteBlog } from './pages/WriteBlog';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
+import { TermsAndConditions } from './pages/TermsAndConditions';
+import { AboutUs } from './pages/AboutUs';
+import { ContactUs } from './pages/ContactUs';
+import { Sitemap } from './pages/Sitemap';
+import { SitemapXml } from './pages/SitemapXml';
+import { BankingKnowledge } from './pages/BankingKnowledge';
+import { CategoryPage } from './pages/CategoryPage';
+import { CreditCardFinder } from './calculators/CreditCardFinder';
+
+// Excel Tool blog section
+import ExcelTool from './pages/ExcelTool';
+import ExcelToolPost from './pages/ExcelToolPost';
+
+// ADD THIS IMPORT
+import ResumeBuilder from './pages/ResumeBuilder';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Layout>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
+    <Layout>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-            {calculatorCategories.flatMap((category) =>
-              category.calculators.map((calculator) => (
-                <Route
-                  key={calculator.id}
-                  path={`/calculators/${calculator.id}`}
-                  element={<CalculatorPage calculatorId={calculator.id} />}
-                />
-              ))
-            )}
+        {calculatorCategories.flatMap(category =>
+          category.calculators.map(calculator => (
+            <Route
+              key={calculator.id}
+              path={`/calculators/${calculator.id}`}
+              element={<CalculatorPage calculatorId={calculator.id} />}
+            />
+          ))
+        )}
 
-            <Route path="/category/:categoryId" element={<CategoryPage />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogPost />} />
-            <Route path="/blog/write" element={<WriteBlog />} />
-            <Route path="/blog/category/banking" element={<BankingKnowledge />} />
-            <Route path="/exceltool" element={<ExcelTool />} />
-            <Route path="/exceltool/:slug" element={<ExcelToolPost />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/sitemap" element={<Sitemap />} />
-            <Route path="/sitemap.xml" element={<SitemapXml />} />
-            <Route path="/credit-card-finder" element={<CreditCardFinder />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </Layout>
-      </TooltipProvider>
-    </QueryClientProvider>
+        <Route path="/category/:categoryId" element={<CategoryPage />} />
+
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/blog/write" element={<WriteBlog />} />
+        <Route path="/blog/category/banking" element={<BankingKnowledge />} />
+
+        {/* Excel Tool blog section routes */}
+        <Route path="/exceltool" element={<ExcelTool />} />
+        <Route path="/exceltool/:slug" element={<ExcelToolPost />} />
+
+        {/* ADD THIS ROUTE */}
+        <Route path="/resume-builder" element={<ResumeBuilder />} />
+
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/sitemap" element={<Sitemap />} />
+        <Route path="/sitemap.xml" element={<SitemapXml />} />
+        <Route path="/credit-card-finder" element={<CreditCardFinder />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Layout>
   );
 }
 
